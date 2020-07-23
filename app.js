53ae348f8222bdc7df7b0e64977b64ca048a46a3
Kevin Song <kevin.song@gsa.gov>
2020-07-23 00:04:06 -0400

    wip

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'gsa-coding-challenge.ccr1debqftur.us-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'rei12345',
+    port: 5432,
+});
+
+const host = 'api.sam.gov';
+const api_key = '9PdfUrRuXuBUsMK0kgApHBn0tyFAlA2VjcV44rRT';
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  await client.connect();
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+  await client.query('select * from opportunities limit 1000').then(data => {
+    res.send(data);
+  });
+
+  await client.end();
+});
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
