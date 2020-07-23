a8dc918bf787edde44f39ccc16957db6c8e395ee
Kevin Song <kevin.song@gsa.gov>
2020-07-23 11:03:45 -0400

    setup skeleton of org awarded amount call

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const api_key = '9PdfUrRuXuBUsMK0kgApHBn0tyFAlA2VjcV44rRT';
 
 const port = 3000
 
-app.get('/', async (req, res) => {
-  await pool.query('select * from opportunities limit 10').then(data => {
-    console.log(data);
-    res.send(data);
-  });
+app.get('/orgAwardedAmount', async (req, res) => {
+  let orgs = req.query.orgs.split(',');
+  let from = req.query.from;
+  let to = req.query.to;
+
+  console.log('Get org awarded amount request', orgs, from, to);
 });
+
+
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
